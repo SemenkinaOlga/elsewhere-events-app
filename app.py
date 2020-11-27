a69757c897d73f99cc0941e7317058f28379fc6a
O.Semenkina <semenkinaolga@gmail.com>
2020-11-28 00:11:14 +0700

    fix: fix plot and bar, added select for column

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 from bokeh.embed import file_html
 from bokeh.resources import CDN
+import numpy as np
 import pandas as pd
 
 import read_data as rd
@@ -19,8 +20,8 @@ server = app.server
 app.config["suppress_callback_exceptions"] = True
 
 PAGE_SIZE = 20
-plot_height = 300
-plot_width = 600
+plot_height = 400
+plot_width = 900
 default_colors = ['red', 'orange', 'lightyellow', 'green', 'darkgreen']
 more_colors = ['red', 'orange', 'yellow', 'green', 'deepskyblue', 'blue', 'darkviolet']
 ten_colors = ['red', 'orange', 'yellow', 'lime', 'green', 'deepskyblue', 'blue', 'indigo', 'deeppink', 'black']
@@ -28,8 +29,20 @@ ten_colors = ['red', 'orange', 'yellow', 'lime', 'green', 'deepskyblue', 'blue',
 additional_columns = ['macro_region', 'meso_region', 'development_level', 'gdp_per_capita',
                       'gdp_volume', 'hdi', 'ECI', 'code', 'country_population_2018', 'country_population_2018_level']
 
+dropdown_options = [{'label': 'Macro Region', 'value': 'macro_region'},
+                    {'label': 'Meso Region', 'value': 'meso_region'},
+                    {'label': 'Development Level', 'value': 'development_level'},
+                    {'label': 'GDP per Capita', 'value': 'gdp_per_capita'},
+                    {'label': 'GDP Volume', 'value': 'gdp_volume'},
+                    {'label': 'HDI', 'value': 'hdi'},
+                    {'label': 'ECI', 'value': 'ECI'},
+                    {'label': 'Country Population 2018 Level', 'value': 'country_population_2018_level'},
+                    {'label': 'Source', 'value': 'source'}]
 
-def create_df_for_map(selected_year_min, selected_year_max, cluster_number, chosen_sources, use_icons, use_clusters):
+dropdown_float_options = ['gdp_per_capita', 'gdp_volume', 'hdi', 'ECI']
+
+
+def create_df_for_map(selected_year_min, selected_year_max, cluster_number, chosen_sources, use_clusters):
     df = df_country_source_year[df_country_source_year['year'] >= selected_year_min]
     df = df[df['year'] <= selected_year_max]
     df = df[df['source'].isin(chosen_sources)]
@@ -50,19 +63,54 @@ def create_df_for_map(selected_year_min, selected_year_max, cluster_number, chos
         return grouped_df
 
 
-def make_map(df_for_map, selected_year_min, selected_year_max, cluster_number, chosen_sources, use_icons, use_clusters):
+def make_map(df_for_map, selected_year_min, selected_year_max, cluster_number, chosen_sources, use_clusters):
     if use_clusters:
         name = "Relative events for countries in [" + str(selected_year_min) + ", " + \
                str(selected_year_max) + "] - KMeans clusters " + str(cluster_number)
-        if use_icons:
-            return mapping.get_ico_map(df_for_map, 'index', name, ten_colors[:cluster_number], True)
         return mapping.get_simple_map(df_for_map, 'index', name, ten_colors[:cluster_number], True)
     else:
         name = "Relative number of events per 1M people in [" + str(selected_year_min) + ", " + \
                             str(selected_year_max) + "]"
-        if use_icons:
-            return mapping.get_ico_map(df_for_map, 'relative_1M_count', name, ten_colors)
         return mapping.get_simple_map(df_for_map, 'relative_1M_count', name, ten_colors)
+
+
+def create_levels(df, column):
+    min_value = df[column].min()
+    max_value = df[column].max()
+
+    steps_number = 10
+    step = (max_value - min_value) / steps_number
+    conditions = []
+    choices = []
+
+    current = min_value
+    current_max = min_value + step
+
+    divider = 1
+    text = ''
+    if max_value > 1000:
+        if max_value > 1000000000:
+            divider = 1000000000
+            text = 'b'
+        elif max_value > 1000000:
+            divider = 1000000
+            text = 'm'
+        else:
+            divider = 1000
+            text = 'k'
+
+    for i in range(0, steps_number):
+        conditions.append((df[column] <= current_max) & (df[column] > current))
+        choices.append(str(round(current/divider, 2)) + text + '-' + str(round(current_max/divider, 2)) + text)
+        current = current + step
+        if i == steps_number - 2:
+            current_max = max_value
+        else:
+            current_max = current_max + step
+
+    df[column + '_level'] = np.select(conditions, choices, default='unknown')
+
+    return df
 
 
 # Read files
@@ -72,12 +120,6 @@ df_country_year = data['country_year']
 df_country_source_year = data['country_source_year']
 df_city_year = data['city_year']
 df_city = data['city']
-
-fig = px.bar(df_country, x="country", y="count", color="macro_region")
-fig_bokeh = plots.get_bar_plot_sum(df_country_year, 'development_level', 'Title', plot_height, plot_width)
-html_bokeh = file_html(fig_bokeh, CDN, "my plot")
-fig_bokeh_2 = plots.get_2_dim_plot_sum(df_country_year, 'year', 'development_level', plot_height, plot_width)
-html_bokeh_2 = file_html(fig_bokeh_2, CDN, "my plot")
 
 years = df_country_source_year['year'].unique().tolist()
 years.sort()
@@ -90,24 +132,25 @@ sources = df_country_source_year['source'].unique().tolist()
 init_sources = ['TED', 'MEETUP', 'BEHANCE', 'EFLUX', 'ART_EDUCATION']
 init_use_ico = False
 
-init_df_for_map = create_df_for_map(min_year, max_year, init_cluster_number, init_sources, init_use_ico, additional_columns)
-init_map = make_map(init_df_for_map, min_year, max_year, init_cluster_number, init_sources, init_use_ico, additional_columns)
+print(df_country_source_year.columns)
+dict_cols_transform_sum = dict((col, lambda x: x.iloc[0]) for col in additional_columns if col in df_country_year.columns)
+dict_cols_transform_sum['count'] = "sum"
+dict_cols_transform_sum['relative_1M_count'] = "sum"
+
+init_df_for_map = create_df_for_map(min_year, max_year, init_cluster_number, init_sources, additional_columns)
+init_map = make_map(init_df_for_map, min_year, max_year, init_cluster_number, init_sources, additional_columns)
 init_map.save(rd.get_relative_path("init_map.html"))
+
+fig_bokeh_bar = plots.get_bar_plot_sum(df_country_year, 'development_level', '', plot_height, plot_width, 'development_level')
+html_bokeh_bar = file_html(fig_bokeh_bar, CDN, "fig_bokeh_bar")
+fig_bokeh_plot = plots.get_2_dim_plot_sum(df_country_year, 'year', 'development_level', plot_height, plot_width, 'development_level')
+html_bokeh_plot = file_html(fig_bokeh_plot, CDN, "fig_bokeh_plot")
 
 app.layout = html.Div(children=[
     html.H1(children='Elsewhere Events Dash'),
-    html.H3(children='''A web application for research.'''),
-
     html.Div(id='map-div', children=[
         html.Div(id='map-settings', children=[
-            dcc.Checklist(
-                id='use-ico-map',
-                options=[
-                    {'label': 'Use icons', 'value': 'ICO'}
-                ],
-                value=[],
-                labelStyle={'display': 'inline-block'}
-            ),
+            html.Br(),
             html.Div(id='chosen-year-label'),
             dcc.RangeSlider(
                 id='year-range-slider',
@@ -119,6 +162,7 @@ app.layout = html.Div(children=[
                        str(max_year): str(max_year),
                        str(avg_year): str(avg_year)}
             ),
+            html.Br(),
             dcc.Checklist(
                 id='use-cluster-map',
                 options=[
@@ -136,6 +180,7 @@ app.layout = html.Div(children=[
                 marks={str(cl): str(cl) for cl in clusters},
                 step=None
             ),
+            html.Br(),
             html.Label('Sources'),
             dcc.Checklist(
                 id='multi-source-map',
@@ -153,27 +198,53 @@ app.layout = html.Div(children=[
             )
         ], style={'width': '68%', 'display': 'inline-block'})
     ]),
-
+    html.Br(),
     html.Div(id='graphs', children=[
-        dcc.Graph(
-            id='example-graph',
-            figure=fig
-        ),
+        html.Div(id='graphs-settings', children=[
+            html.Br(),
+            html.Label('Color'),
+            dcc.Dropdown(
+                id='graphs-color',
+                options=dropdown_options,
+                value='development_level',
+                clearable=False
+            )
+        ], style={'width': '18%', 'float': 'left', 'display': 'inline-block'}),
+        html.Div(id='graphs-plot', children=[
+            html.Iframe(
+                id="bokeh_bar",
+                srcDoc=html_bokeh_bar,
+                width=plot_width * 1.1,
+                height=plot_height * 1.1
+            ),
+            html.Iframe(
+                id="bokeh_plot",
+                srcDoc=html_bokeh_plot,
+                width=plot_width * 1.1,
+                height=plot_height * 1.1
+            )
+        ], style={'width': '78%', 'display': 'inline-block'})
 
-        html.Iframe(
-            srcDoc=html_bokeh,
-            width=plot_width * 1.1,
-            height=plot_height * 1.1
-        ),
-
-        html.Iframe(
-            srcDoc=html_bokeh_2,
-            width=plot_width * 1.1,
-            height=plot_height * 1.1
-        )
     ],
         style={'width': '100%', 'display': 'inline-block'}),
 ])
+
+
+@app.callback(
+    Output('bokeh_bar', 'srcDoc'),
+    Output('bokeh_plot', 'srcDoc'),
+    [Input('graphs-color', 'value')])
+def update(color):
+    current_df = df_country_source_year
+    n_color = color
+    if color in dropdown_float_options:
+        current_df = create_levels(current_df, color)
+        n_color = color + '_level'
+    bokeh_bar = plots.get_bar_plot_sum(current_df, n_color, '', plot_height, plot_width, color)
+    html_bar = file_html(bokeh_bar, CDN, "fig_bokeh_bar")
+    bokeh_plot = plots.get_2_dim_plot_sum(current_df, 'year', n_color, plot_height, plot_width, color)
+    html_plot = file_html(bokeh_plot, CDN, "fig_bokeh_plot")
+    return html_bar, html_plot
 
 
 @app.callback(
@@ -182,11 +253,10 @@ app.layout = html.Div(children=[
     [Input('year-range-slider', 'value'),
      Input('cluster-slider-map', 'value'),
      Input('multi-source-map', 'value'),
-     Input('use-ico-map', 'value'),
      Input('use-cluster-map', 'value')])
-def update(year_range, cluster_number, chosen_sources, use_icons, use_clusters):
-    df_for_map = create_df_for_map(year_range[0], year_range[1], cluster_number, chosen_sources, use_icons, use_clusters)
-    current_map = make_map(df_for_map, year_range[0], year_range[1], cluster_number, chosen_sources, use_icons, use_clusters)
+def update(year_range, cluster_number, chosen_sources, use_clusters):
+    df_for_map = create_df_for_map(year_range[0], year_range[1], cluster_number, chosen_sources, use_clusters)
+    current_map = make_map(df_for_map, year_range[0], year_range[1], cluster_number, chosen_sources, use_clusters)
     current_map.save(rd.get_relative_path("map.html"))
     year_label = 'Year [' + str(year_range[0]) + ", " + str(year_range[1]) + "]"
     return open(rd.get_relative_path("map.html"), 'r').read(), year_label
--- plots.py
+++ plots.py
@@ -1,9 +1,15 @@
 from bokeh.plotting import figure
+from bokeh.models import ColorBar, ColumnDataSource
+from bokeh.models import LinearColorMapper
+from bokeh.transform import linear_cmap
 from bokeh.palettes import magma
 from bokeh.palettes import viridis
 from bokeh.palettes import plasma
+from bokeh.models.tools import HoverTool
+from bokeh.core.properties import value
 from bokeh.models import Legend
 import pandas as pd
+import math
 
 
 def get_2_dim_plot_count(df, x_name, column, height, width):
@@ -16,7 +22,7 @@ def get_2_dim_plot_count(df, x_name, column, height, width):
     p = figure(plot_width=width, plot_height=height)
     palette = viridis(len(unique_values))
 
-    i = 0;
+    i = 0
     for val in unique_values:
         current = grouped_df[grouped_df[column] == val]
         r1 = p.line(current[x_name], current['count'], line_width=1, color=palette[i])
@@ -36,9 +42,18 @@ def get_2_dim_plot_count(df, x_name, column, height, width):
     return p
 
 
-def get_2_dim_plot_sum(df, x_name, column, height, width):
-    grouped_df = df.groupby([column, x_name])
-    grouped_df = pd.DataFrame(grouped_df.sum().reset_index())
+def get_2_dim_plot_sum(df, x_name, column, height, width, sort_col):
+    cols_for_groups = [column, x_name, 'count']
+    if sort_col != column:
+        cols_for_groups.append(sort_col)
+    grouped_df = df[cols_for_groups].groupby([column, x_name])
+
+    if sort_col != column:
+        grouped_df = pd.DataFrame(grouped_df.agg({sort_col: lambda x: x.iloc[0], 'count': 'sum'}).reset_index())
+    else:
+        grouped_df = pd.DataFrame(grouped_df.sum().reset_index())
+
+    grouped_df = grouped_df.sort_values(by=[x_name, sort_col])
 
     legend_items = []
 
@@ -46,7 +61,7 @@ def get_2_dim_plot_sum(df, x_name, column, height, width):
     p = figure(plot_width=width, plot_height=height)
     palette = viridis(len(unique_values))
 
-    i = 0;
+    i = 0
     for val in unique_values:
         current = grouped_df[grouped_df[column] == val]
         r1 = p.line(current[x_name], current['count'], line_width=1, color=palette[i])
@@ -55,13 +70,11 @@ def get_2_dim_plot_sum(df, x_name, column, height, width):
         i = i + 1
 
     legend = Legend(items=legend_items, location=(0, -30))
-
     p.add_layout(legend, 'right')
-
     p.legend.click_policy = "hide"
 
     p.xaxis.axis_label = x_name
-    p.yaxis.axis_label = 'Sum'
+    p.yaxis.axis_label = 'Sum of events'
 
     return p
 
@@ -82,17 +95,46 @@ def get_bar_plot_count(df, column, title, height, width):
     return p
 
 
-def get_bar_plot_sum(df, column, title, height, width):
-    grouped_df = df.groupby([column])
-    grouped_df = pd.DataFrame(grouped_df.sum().reset_index())
+def get_bar_plot_sum(df, column, title, height, width, sort_col):
+    cols_for_groups = [column, 'count']
+    if sort_col != column:
+        cols_for_groups.append(sort_col)
+    grouped_df = df[cols_for_groups].groupby([column])
+
+    if sort_col != column:
+        grouped_df = pd.DataFrame(grouped_df.agg({sort_col: lambda x: x.iloc[0], 'count': 'sum'}).reset_index())
+    else:
+        grouped_df = pd.DataFrame(grouped_df.sum().reset_index())
+
+    grouped_df = grouped_df.sort_values(by=[sort_col])
 
     unique_values = grouped_df[column].unique().tolist()
     palette = viridis(len(unique_values))
 
-    p = figure(x_range=unique_values, plot_height=height, plot_width=width, title=title)
+    source = ColumnDataSource(data=dict(col=grouped_df[column], count=grouped_df['count'], color=palette))
 
-    p.vbar(x=grouped_df[column], top=grouped_df['count'], width=0.9, color=palette, fill_alpha=.75)
+    p = figure(x_range=unique_values, title=title, width=width, height=height)
 
-    p.yaxis.axis_label = "Sum"
+    p.vbar(x='col', top='count', color='color', source=source, width=0.5, legend_field='col')
+
+    p.xaxis.axis_label = ''
+    p.yaxis.axis_label = 'Sum of events'
+
+    p.xgrid.grid_line_color = None
+
+    hover = HoverTool()
+    hover.tooltips = [
+        ("Type", '@col'),  # $name provides data from legend
+        ("Value", '@count')  # @$name gives the value corresponding to the legend
+    ]
+    p.add_tools(hover)
+
+    if len(unique_values) > 5:
+        p.xaxis.major_label_orientation = math.pi / 2  # so that the labels do not come on top of each other
+
+    new_legend = p.legend[0]
+    # p.legend[0].plot = None
+    p.add_layout(new_legend, 'right')
+    p.legend.click_policy = "hide"
 
     return p
